55efaead453748ef1549cd314488a1e6b4a6d7a0
Kacper Rusinek <rusinekkacper@wp.pl>
2023-06-30 10:29:53 +0200

    CreateBook

---
+++ BookStore/Models/CreateBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Models
+{
+    public class CreateBook
+    {
+        public string Title { get; set; }
+        public string FirstNameOfAuthor  { get; set; }
+        public string LastNameOfAuthor  { get; set; }
+        public string Species  { get; set; }
+        public int PublicationYear  { get; set; }
+        public DateTime PublicationDate { get; set; } 
+    }
+}
